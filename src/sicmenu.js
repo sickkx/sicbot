234c46a4b57a89de5e1debd344d2e377e573ff1f
sicbot <davidluisfsa05@gmail.com>
2021-02-14 22:17:05 -0300

    First commit

---
+++ src/sicmenu.js
@@ -0,0 +1,12 @@
+const sicmenu = (prefix, pushname) => {
+    return `◪ *Comandos do sic*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.sicmenu = sicmenu
